014a8e9473fe45063167d33c00f5d389e9b84441
Eric Sexton <foopis23@gmail.com>
2021-03-21 12:26:57 -0400

    Interface for rotating ai

---
+++ Assets/Scripts/AI/SimpleLerpRotation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SimpleLerpRotation : MonoBehaviour, IRotation
+{
+    [SerializeField]
+    private float rotationDamping;
+
+    private Vector3 _targetPos;
+
+    public void Update()
+    {
+        var lookDirection = _targetPos - transform.position;
+        lookDirection.y = 0;
+        var rotation = Quaternion.LookRotation(lookDirection);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationDamping);
+    }
+    
+    public void RotateTowards(Vector3 targetPos)
+    {
+        _targetPos = targetPos;
+    }
+}
